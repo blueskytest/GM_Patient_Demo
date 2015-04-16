e0f19916ee65d3f770f34474ceaa1ed9348b727a
blueskytest <greg.mckenna@e-mis.com>
2015-04-16 09:12:31 +0100

    Updated Patient Demo App GM
    Changes made for mentoring class #2

---
+++ Mentoring/PatientDemo/PatientDemo.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using PatientDemo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientDemo.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Patient myPatient = new Patient();
+			myPatient.Surname = "Smith";
+			myPatient.Forename = "John";
+			myPatient.Sex = "Male";
+
+
+			Console.WriteLine("Name: " + myPatient.Forename + " " + myPatient.Surname);
+			Console.WriteLine("Sex: " + myPatient.Sex);
+
+			Console.Write("What is patient height (cm)? ");
+			String heightAsString = Console.ReadLine();
+			int Height = int.Parse(heightAsString);
+
+
+			Console.Write("What is patient weight (kg)? ");
+			String weightAsString = Console.ReadLine();
+			int Weight = int.Parse(weightAsString);
+
+
+			var BMI = Weight / (Height * Height);
+
+			Console.Write("Patient BMI = " + BMI.ToString() + "%");
+
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Mentoring/PatientDemo/PatientDemo.Domain/Entities/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace PatientDemo.Domain.Entities
+{
+    public class Patient
+    {
+        public string Surname { get; set; }
+        public string Forename { get; set; }
+		public string Sex { get; set; }
+		public string Height { get; set; }
+		public string Weight { get; set; }
+		public string BMI { get; set; }
+    }
+}
